d9eac919dee0cb0ef6730aa9228683a5f7651f11
Alejoooooo <raponialessio@hotmail.com>
2016-12-22 18:24:21 +0100

    Checked registration password and created error page

---
+++ error.php
@@ -0,0 +1,25 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+    rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css"
+    rel="stylesheet" type="text/css">
+  </head>
+    
+  <body>
+
+<?php
+	include 'script/template_menu.php';
+?>
+
+<h1>ERROR</h1>
+
+
+</body>
+</html>
--- script/php/register.php
+++ script/php/register.php
@@ -1,12 +1,24 @@
 <?php
+
+
 require 'dbconnection/DBAccessManager.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
+$confirm_password = $_POST['confirm-password'];
 $email = $_POST['email'];
 
+if($password != $confirm_password)
+    header('Location: ../../error.php');
+
+else {
 $parameters = array('username' => $username, 'password' => hash('sha512', $password), 'email' => $email);
 
 $query = 'insert into utente (id, username, password, email) values(\N, :username, :password, :email)';
 $dbAM = new DBAccessManager();
 $dbAM->insert($parameters, $query);
+
+$newURL = "/chefcost";
+header('Location: '.$newURL);
+}
+
 ?>
